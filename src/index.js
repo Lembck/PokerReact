0681aa08d865f1db6b5d1b474aa22f8fb2ed13a8
Michael Lembck <lembckm@gmail.com>
2021-06-03 22:43:11 -0400

    adding images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function Square(props) {
+  const mystyle = {
+    border: "solid 1px black",
+    width: "50px",
+    height: "60px",
+    float: "left",
+  };
+
+  return (
+    <div style={mystyle}>
+      <img src={require("card.png")} alt={props.char} width="50" height="60">
+      </img>
+    </div>
+  )
+}
+
+class Board extends React.Component {
+  renderSquare(char, suit) {
+    return (
+      <Square
+        char={char}
+        suit={suit}
+      />
+    );
+  }
+
+  render() {
+    const items = []
+
+    for (const [suitIndex, suitValue] of this.props.suits.entries()) {
+      for (const [suitIndex, charValue] of this.props.chars.entries()) {
+        items.push(this.renderSquare(charValue, suitValue))
+      }
+    }
+
+    return (
+      <div style={{width: "676px"}}>
+        {items}
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.suits = ["clubs", "diamonds", "hearts", "spades"];
+    this.chars = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            chars={this.chars}
+            suits={this.suits}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
